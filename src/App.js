c65b64bf1139c17532f149cda239a12b263e40be
jcont12 <jorgeacontrerasl@gmail.com>
2017-08-31 18:06:16 -0700

    Add images, add text and logo to nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header'
+
 class App extends Component {
   render() {
     return (
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import '../App.css';
 
 class Header extends Component {
   render() {
     return (
       <header>
-      Test
+        <div className="logo">
+          <img src={require('../images/logo.png')} alt='Chantli-Mare-logo' />
+        </div>
       </header>
     );
   }
