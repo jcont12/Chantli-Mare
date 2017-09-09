681bba81c25b9e4865d297bfe5c15da95d873752
jcont12 <jorgeacontrerasl@gmail.com>
2017-09-09 10:22:17 -0700

    Add rest of body to dinning, including desserts and lunch menu

--- src/components/pages/dining/menu.js
+++ src/components/pages/dining/menu.js
@@ -5,65 +5,79 @@ class Menu extends Component{
   constructor() {
     super()
       this.state = {
-        menu: 'lunch'
+        menu: 'dinner'
       };
     }
 
   render(){
-    if (this.state.menu === 'lunch'){
+    if (this.state.menu === 'dinner'){
     return(
       <div className='dining-menu'>
-        Lunch
+        <h3>'Dinner'</h3><h3>'Lunch'</h3>
+        <h4>'Entrees'</h4>
         <table>
           <tr>
-            <td class="meal">Seafood Soup</td>
+            <td class="meal">'Seafood Soup'</td>
             <td></td>
-            <td class="meal">Dorado with Serrano Chili and Tequila Butter</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Pargo, Red Snapper and Shrimp served with Garlic Pesto Baguette</td>
-            <td></td>
-            <td>Zapped with Fresh Lime, Agave Honey and Roasted Peppers</td>
+            <td class="meal">'Dorado with Serrano Chili and Tequila Butter'</td>
             <td></td>
           </tr>
           <tr>
-            <td class="meal"> Chantli House Salad</td>
+            <td>'Pargo, Red Snapper and Shrimp served with Garlic Pesto Baguette'</td>
             <td></td>
-            <td class="meal">Zarandeado Grilled Salmon Salad</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Spinach, Romaine Lettuce, Strawberrys, Caramel Peckan, Goat Cheese, and Balsamic and Honey Vinagrette</td>
-            <td></td>
-            <td>With Lime and Avocado Alioli</td>
+            <td>'Zapped with Fresh Lime, Agave Honey and Roasted Peppers'</td>
             <td></td>
           </tr>
           <tr>
-            <td class="meal">Cesario Mexicana</td>
+            <td class="meal">'Chantli House Salad'</td>
             <td></td>
-            <td class="meal">Steak Whiskey Saigon Style</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Romaine Lettuce leaves in our Garlicky House Caesar Dressing with Bacon, and Tortilla strips
-            Add grilled chicken breast - $130
-            Add Srimp - $140</td>
-            <td></td>
-            <td>Imported Vacio Skirt Steak grilled and served with Peppercorn, Bourbon Glacee, Buttered Vegetables and Yam Mash</td>
+            <td class="meal">'Zarandeado Grilled Salmon Salad'</td>
             <td></td>
           </tr>
           <tr>
-            <td class="meal">Tuna Tartare</td>
+            <td>'Spinach, Romaine Lettuce, Strawberrys, Caramel Peckan, Goat Cheese, and Balsamic and Honey Vinagrette'</td>
             <td></td>
-            <td class="meal">Tuna Ashinaga</td>
+            <td>'With Lime and Avocado Alioli'</td>
             <td></td>
           </tr>
           <tr>
-            <td>Saku Pink Sushi Grade Tuna chopped raw and layered with Avocado, Green Onion, and Watermelon, Drizzled with Serrano Chili spiked Soy Sauce</td>
+            <td class="meal">'Cesario Mexicana'</td>
             <td></td>
-            <td>Top grade Sushi Tuna seared rare and served with a Miso, Green, Onion and Sake Cream Sauce, Vegetable Stir Fry and Jasmin Rice</td>
+            <td class="meal">'Steak Whiskey Saigon Style'</td>
             <td></td>
+          </tr>
+          <tr>
+            <td>'Romaine Lettuce leaves in our Garlicky House Caesar Dressing with Bacon, and Tortilla strips\nAdd grilled chicken breast - $130  Add Srimp - $140'</td>
+            <td></td>
+            <td>'Imported Vacio Skirt Steak grilled and served with Peppercorn, Bourbon Glacee, Buttered Vegetables and Yam Mash'</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td class="meal">'Tuna Tartare'</td>
+            <td></td>
+            <td class="meal">'Tuna Ashinaga'</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>'Saku Pink Sushi Grade Tuna chopped raw and layered with Avocado, Green Onion, and Watermelon, Drizzled with Serrano Chili spiked Soy Sauce'</td>
+            <td></td>
+            <td>'Top grade Sushi Tuna seared rare and served with a Miso, Green, Onion and Sake Cream Sauce, Vegetable Stir Fry and Jasmin Rice'</td>
+            <td></td>
+          </tr>
+        </table>
+        <h4>'Desserts'</h4>
+        <table>
+          <tr>
+            <td class="meal">'Vanilla Ice Cream Sundae'</td>
+            <td class="meal">'Creme Brulee of the day'</td>
+          </tr>
+          <tr>
+            <td>Topped with berries</td>
+            <td>Ask your waiter</td>
+          </tr>
+          <tr>
+            <td class="meal">'Chocolate Cake'</td>
+            <td class="meal">'Banana Foster au Rum'</td>
           </tr>
         </table>
       </div>
@@ -71,8 +85,67 @@ class Menu extends Component{
   } else {
       return(
         <div className='dining-menu'>
-          Dinner
-        </div>
+        Lunch
+        <table>
+          <tr>
+            <td class="meal">'Mediterranean Tomato and Seafood Soup'</td>
+            <td></td>
+            <td class="meal">'Croque Señor'</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>'Served with Fish, Shrimp, and Garlic Baguette'</td>
+            <td></td>
+            <td>'Ham, Gouda Cheese, Sauteed Mushrooms and Onions, grilled and served on an English Muffin'</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td class="meal">'Guacamole'</td>
+            <td></td>
+            <td class="meal">'Hamburger Deluxe (8 oz.)'</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>'Served with Tortilla Chips and Pico de Gallo Salsa'</td>
+            <td></td>
+            <td>'Served with Bacon, Choice of Blue or Cheddar Cheese, Sauteed Onions and Mushrooms, with Fried Potatoes'</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td class="meal">'Cesario Mexicana'</td>
+            <td></td>
+            <td class="meal">'Aguachile with Shrimp'</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>'Romaine Lettuce leaves in our Garlicky House Caesar Dressing with Bacon, and Tortilla strips\nAdd grilled chicken breast - $130  Add Srimp - $140'</td>
+            <td></td>
+            <td>'Cucumber and Chile Serrano with Tomatillo and Lime Juice, served ice cold with Tostadas'</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td class="meal">'Pasta al Diablo'</td>
+          </tr>
+          <tr>
+            <td>'Served with Gorgonzola, White Wine, Cream, Tomatoes, Sweet Pepper, Spinach, and a touch of Chile'</td>
+            <td></td>
+          </tr>
+          <h4>'Desserts'</h4>
+          <table>
+            <tr>
+              <td class="meal">'Vanilla Ice Cream'</td>
+              <td class="meal">'Creme Brulee of the day'</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Ask your waiter</td>
+            </tr>
+            <tr>
+              <td class="meal">'Chocolate Cake'</td>
+            </tr>
+          </table>
+        </table>
+      </div>
       )
     }
   }
