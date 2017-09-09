a796c7d870aa7c84196fb03c5ddc7dee9edf48b6
jcont12 <jorgeacontrerasl@gmail.com>
2017-09-08 22:05:57 -0700

    Add blocks without style to home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import '../../App.css';
+import BlockLeft from './home/blockLeft';
+import BlockCenter from './home/blockCenter';
+import BlockRight from './home/blockRight';
 
 class Home extends Component {
   render() {
       return (
         <div className='home'>
-          Hooome
+          <h1> "Experience a relaxing stay in one of Mexico's most beautiful coasts. Our intimate boutique hotel is the perfect refuge from your troubles" </h1>
+          <BlockLeft />
+          <BlockCenter />
+          <BlockRight />
+
         </div>
       );
     }
---
+++ src/components/pages/home/blockRight.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../../../App.css';
+import { Link } from 'react-router-dom';
+
+class BlockRight extends Component {
+  render() {
+    return(
+      <div className='home-block'>
+      <h3> "Discover the Coast" </h3>
+      <img className='home-block' src={require('../../../images/Every_day_a_Feast.jpg')} />
+      <p>"Mexico's Costalegre is renowned for its virgin soft sandy beaches. There is always something to keep you busy, wether it is El Tamarindo's golf course throught the rainforest, nature trails through the mangroves, whale watching, surfing lessons or a relaxing massage. The hotels surroundings over a trove of experiences for our guests."</p>
+      <Link to='/Activities' className='nav-options'> Activities > </Link>
+    </div>
+    )
+  }
+}
+
+export default BlockRight
