1319797bb362342e03f75eb0b5de4b9d22f01f5a
jcont12 <jorgeacontrerasl@gmail.com>
2017-09-11 18:13:30 -0700

    Add map to contactMap

--- src/components/pages/contactComponents/contactMap.js
+++ src/components/pages/contactComponents/contactMap.js
@@ -5,7 +5,12 @@ class ContactMap extends Component {
   render() {
     return (
       <div className='contact-map'>
-        <img src={require('../../../images/Flowers.jpg')}/>
+      <iframe
+        width="600"
+        height="450"
+        frameborder="0"
+        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBNAOcju09MwJMjl6a62_PGS3RTzV_m65k&q=Chantli+Mare" allowfullscreen>
+        </iframe>
       </div>
     )
   }
