2228e52fe30eb2ea5823128db7a98461d90cb82c
jcont12 <jorgeacontrerasl@gmail.com>
2017-09-17 15:50:14 -0700

    Finish styling home page

--- src/components/pages/homeComponents/gallery.js
+++ src/components/pages/homeComponents/gallery.js
@@ -6,10 +6,12 @@ class HomeGallery extends Component {
   render() {
     return (
       <div className='home-gallery'>
-        <h3> Gallery </h3>
-        <p> Visit our gallery and discover what makes us great </p>
-        <Link to='/Gallery' className='nav-options'> Our Photos > </Link>
-        <img src={require('../../../images/nightview2.jpg')} />
+        <div id='home-gallery-intro'>
+          <h3> Gallery </h3>
+          <p> Visit our gallery and discover what makes us great </p>
+          <Link to='/Gallery' className='nav-options'> Our Photos > </Link>
+        </div>
+        <img id='home-gallery-img' src={require('../../../images/nightview2.jpg')} />
         </div>
     )
   }
