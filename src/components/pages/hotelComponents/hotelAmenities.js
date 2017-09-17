3a4ec981e07898ac730e042b2dc90eaa5610feb1
jcont12 <jorgeacontrerasl@gmail.com>
2017-09-17 16:47:41 -0700

    Add styling to amenities pictures in hotel page

--- src/components/pages/hotelComponents/hotelAmenities.js
+++ src/components/pages/hotelComponents/hotelAmenities.js
@@ -5,11 +5,13 @@ class HotelAmenities extends Component {
   render() {
     return (
       <div className='hotel-amenities'>
-      <h3> 'Amenities' </h3>
-        <img className='amenities-pictures' src={require('../../../images/Flowers.jpg')} />
-        <img className='amenities-pictures' src={require('../../../images/Food1.jpg')} />
-        <img className='amenities-pictures' src={require('../../../images/Food2.jpg')} />
-        <img className='amenities-pictures' src={require('../../../images/Food3.jpg')} />
+      <h3> Amenities </h3>
+        <div id='amenities-block-imgs'>
+          <img className='amenities-img' src={require('../../../images/Flowers.jpg')} />
+          <img className='amenities-img' src={require('../../../images/Food1.jpg')} />
+          <img className='amenities-img' src={require('../../../images/Food2.jpg')} />
+          <img className='amenities-img' src={require('../../../images/Food3.jpg')} />
+        </div>
       </div>
     )
   }
