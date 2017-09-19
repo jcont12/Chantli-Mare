6b72ace30b78e3905e138035306e4a5664a2fe4f
Jorge Contreras <jorgeacontrerasl@gmail.com>
2017-09-18 19:37:29 -0700

    Add finishing touches to activity styling

--- src/components/pages/activitiesComponents/atSea.js
+++ src/components/pages/activitiesComponents/atSea.js
@@ -6,14 +6,14 @@ class AtSea extends Component {
     return(
       <div className='activities-atSea'>
         <h3> At Sea </h3>
-        <img id='activities-img'src={require('../../../images/Coconuts.jpg')}/>
         <div id='activities-p'>
-          <p><bold>Whale Watching</bold> We can arrange a boat tour, the winter months are perfect for Humpback whales</p>
-          <p><bold>Kayaking</bold> Take in the sights of our protected bay</p>
-          <p><bold>Surfing</bold>There are a number of breaks close by. Let us know and we can gladly arrange for lessons and board rentals.</p>
-          <p><bold>Fishing</bold>Boat tours can take you for open ocean fishing</p>
-          <p><bold>Snorkeling and Scuba Diving</bold>Enjoy trips to nearby reefs and islands.</p>
+          <p><b>Whale Watching</b> We can arrange a boat tour, the winter months are perfect for Humpback whales</p>
+          <p><b>Kayaking</b> Take in the sights of our protected bay</p>
+          <p><b>Surfing</b> There are a number of breaks close by. Let us know and we can gladly arrange for lessons and board rentals.</p>
+          <p><b>Fishing</b> Boat tours can take you for open ocean fishing</p>
+          <p><b>Snorkeling and Scuba Diving</b> Enjoy trips to nearby reefs and islands.</p>
         </div>
+        <img id='activities-img'src={require('../../../images/Coconuts.jpg')}/>
       </div>
     )
   }
