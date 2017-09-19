3d04db3e425b8e34ec21bafc108650bf87a3a134
Jorge Contreras <jorgeacontrerasl@gmail.com>
2017-09-18 19:30:49 -0700

    Add basic styling to activities page

--- src/components/pages/activitiesComponents/atSea.js
+++ src/components/pages/activitiesComponents/atSea.js
@@ -6,12 +6,14 @@ class AtSea extends Component {
     return(
       <div className='activities-atSea'>
         <h3> At Sea </h3>
-        <p><bold>Whale Watching</bold> We can arrange a boat tour, the winter months are perfect for Humpback whales</p>
-        <p><bold>Kayaking</bold> Take in the sights of our protected bay</p>
-        <p><bold>Surfing</bold>There are a number of breaks close by. Let us know and we can gladly arrange for lessons and board rentals.</p>
-        <p><bold>Fishing</bold>Boat tours can take you for open ocean fishing</p>
-        <p><bold>Snorkeling and Scuba Diving</bold>Enjoy trips to nearby reefs and islands.</p>
-        <img src={require('../../../images/Coconuts.jpg')}/>
+        <img id='activities-img'src={require('../../../images/Coconuts.jpg')}/>
+        <div id='activities-p'>
+          <p><bold>Whale Watching</bold> We can arrange a boat tour, the winter months are perfect for Humpback whales</p>
+          <p><bold>Kayaking</bold> Take in the sights of our protected bay</p>
+          <p><bold>Surfing</bold>There are a number of breaks close by. Let us know and we can gladly arrange for lessons and board rentals.</p>
+          <p><bold>Fishing</bold>Boat tours can take you for open ocean fishing</p>
+          <p><bold>Snorkeling and Scuba Diving</bold>Enjoy trips to nearby reefs and islands.</p>
+        </div>
       </div>
     )
   }
