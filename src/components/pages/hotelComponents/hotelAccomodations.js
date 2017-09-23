12e76f60e71096bbcb4207e32d1a04155eec80f1
jcont12 <jorgeacontrerasl@gmail.com>
2017-09-22 22:43:39 -0700

    Stylize testimonials page

--- src/components/pages/hotelComponents/hotelAccomodations.js
+++ src/components/pages/hotelComponents/hotelAccomodations.js
@@ -8,19 +8,19 @@ class HotelAccomodations extends Component {
         <h3> Accomodations </h3>
         <table id='hotel-accomodations-table'>
         	<tr>
-        	  <th> Master Suites </th>
-        	  <th> Ocean Suites </th>
-        	  <th> Patio Room </th>
+        	  <th className='hotel-accomodations-th'> Master Suites </th>
+        	  <th className='hotel-accomodations-th'> Ocean Suites </th>
+        	  <th className='hotel-accomodations-th'> Patio Room </th>
         	</tr>
         	<tr>
-        	  <td> Large ocean-facing suites with beach views. Large outdoor terrace for dining and relaxing. King size bed, TV, A/C, Large Bathroom. (2 Master Suites Available)</td>
-        	  <td> Ocean-facing suites with beach views. Outdoor terrace with views to the palm grove. King size bed, TV, A/C. (3 Ocean Suites Available) </td>
-        	  <td> Interior Room facing interior patio. King size bed (or 3 single beds upon request), TV, A/C. (1 Patio Room Available)</td>
+        	  <td className='hotel-accomodations-td'> Large ocean-facing suites with beach views. Large outdoor terrace for dining and relaxing. King size bed, TV, A/C, Large Bathroom. (2 Master Suites Available)</td>
+        	  <td className='hotel-accomodations-td'> Ocean-facing suites with beach views. Outdoor terrace with views to the palm grove. King size bed, TV, A/C. (3 Ocean Suites Available) </td>
+        	  <td className='hotel-accomodations-td'> Interior Room facing interior patio. King size bed (or 3 single beds upon request), TV, A/C. (1 Patio Room Available)</td>
         	</tr>
           <tr>
-            <td> <img className='td-image' src={require('../../../images/Room1.jpg')} /></td>
-            <td> <img className='td-image'src={require('../../../images/Room2.jpg')} /></td>
-            <td></td>
+            <td className='hotel-accomodations-td'> <img className='td-image' src={require('../../../images/Room1.jpg')} /></td>
+            <td className='hotel-accomodations-td'> <img className='td-image'src={require('../../../images/Room2.jpg')} /></td>
+            <td className='hotel-accomodations-td'></td>
           </tr>
         </table>
       </div>
--- src/components/pages/testimonialsComponents/customerReviews.js
+++ src/components/pages/testimonialsComponents/customerReviews.js
@@ -5,26 +5,26 @@ class CustomerReviews extends Component {
   render() {
     return(
       <div className='testimonials-reviews'>
-        <table>
+        <table id='testimonials-reviews-table'>
           <tr>
-            <td>'“We looked for a place to relax and get off the daly stress. Our prerequisites were calm, sun, sea and edge quality . At Chantly Mare, reality exceeded our expectations. The friendliness , the personalized attention of Don Roberto and his team made our 9 day stay a real success.”'</td>
-            <td>'“ It should be evident then that the ambience at the Boutique Inn was addictive. The attention to all things by Roberto (the owner) Chef Greg, and his smiling and very competent staff, simply made it one of the most comfortable places we have encountered over many years. Add to this the attractiveness of the area, the 6 km beach, the lovely trappings beneath the palapa, and all of it so well kept. Exceptional details of design and comfort seem to be something we expected after meeting Roberto and so it was found to be.”'</td>
-            <td>'“We really loved our stay in Chantli Mare and I think it is the most beautifully designed hotel I have stayed in so far. The atmosphere is wonderful. Everything is perfect - the rooms, the restaurant, the pool area. The owner is a very friendly man who really takes good care of his guests.”'</td>
+            <td className='testimonials-reviews-td-testimonial'>“We looked for a place to relax and get off the daily stress. Our prerequisites were calm, sun, sea and edge quality . At Chantly Mare, reality exceeded our expectations. The friendliness , the personalized attention of Don Roberto and his team made our 9 day stay a real success.”</td>
+            <td className='testimonials-reviews-td-testimonial'>“ It should be evident then that the ambience at the Boutique Inn was addictive. The attention to all things by Roberto (the owner) Chef Greg, and his smiling and very competent staff, simply made it one of the most comfortable places we have encountered over many years. Add to this the attractiveness of the area, the 6 km beach, the lovely trappings beneath the palapa, and all of it so well kept. Exceptional details of design and comfort seem to be something we expected after meeting Roberto and so it was found to be.”</td>
+            <td className='testimonials-reviews-td-testimonial'>“We really loved our stay in Chantli Mare and I think it is the most beautifully designed hotel I have stayed in so far. The atmosphere is wonderful. Everything is perfect - the rooms, the restaurant, the pool area. The owner is a very friendly man who really takes good care of his guests.”</td>
           </tr>
           <tr>
-            <td>'— TripAdvisor review by brngt from Paris'</td>
-            <td>'— TripAdvisor review by alanmacAlberta'</td>
-            <td>'— Tripadvisor review by Travellinggypsies from Germany'</td>
+            <td className='testimonials-reviews-td-author'>— TripAdvisor review by brngt from Paris</td>
+            <td className='testimonials-reviews-td-author'>— TripAdvisor review by alanmacAlberta</td>
+            <td className='testimonials-reviews-td-author'>— Tripadvisor review by Travellinggypsies from Germany</td>
           </tr>
           <tr>
-            <td>'“I spent 9 relaxing days in chantli mare and i must say it could have been more! The beach and jungle around are spectacular. I enjoyed every meal there ( and yes i almost tried everything in the menu because i just didnt find any reason to eat in another place), the cook did a fantastic job. Staff was friendly and helpful, the owner helped is to organize trips. Thanks for an unforgettable time in chantli mare! I recommend it to anyone.”'</td>
-            <td>'“This place is a gem as you walk 10 feet out the door of your room to a beach that feels almost private. There is nothing touristy about this place. The little dirt road that leads you to this 6 room B&B brings you nothing but surprise and delight. The rooms and property are spotless - even after the hurricane. The food is the best around. You have to try to the pesto omelette! Roberto loves this place and it shows.”'</td>
-            <td>'“This is definitely a hidden gem! The setting is beautiful, the owner, Roberto, couldn’t have been more attentive and the food was WONDERFUL! The rooms were very clean and comfortable. We only planned to stay one night but enjoyed our stay so much, we added another night. I would definitely recommend this boutique hotel for a family or for a romantic get-away!”'</td>
+            <td className='testimonials-reviews-td-testimonial'>“I spent 9 relaxing days in chantli mare and i must say it could have been more! The beach and jungle around are spectacular. I enjoyed every meal there ( and yes i almost tried everything in the menu because i just didnt find any reason to eat in another place), the cook did a fantastic job. Staff was friendly and helpful, the owner helped is to organize trips. Thanks for an unforgettable time in chantli mare! I recommend it to anyone.”</td>
+            <td className='testimonials-reviews-td-testimonial'>“This place is a gem as you walk 10 feet out the door of your room to a beach that feels almost private. There is nothing touristy about this place. The little dirt road that leads you to this 6 room B&B brings you nothing but surprise and delight. The rooms and property are spotless - even after the hurricane. The food is the best around. You have to try to the pesto omelette! Roberto loves this place and it shows.”</td>
+            <td className='testimonials-reviews-td-testimonial'>“This is definitely a hidden gem! The setting is beautiful, the owner, Roberto, couldn’t have been more attentive and the food was WONDERFUL! The rooms were very clean and comfortable. We only planned to stay one night but enjoyed our stay so much, we added another night. I would definitely recommend this boutique hotel for a family or for a romantic get-away!”</td>
           </tr>
           <tr>
-            <td>'— TripAdvisor review by Nina S from Switzerland'</td>
-            <td>'— TripAdvisor review by brodis01 from USA'</td>
-            <td>'— TripAdvisor review by merrileej072052 from Houston'</td>
+            <td className='testimonials-reviews-td-author'>— TripAdvisor review by Nina S from Switzerland</td>
+            <td className='testimonials-reviews-td-author'>— TripAdvisor review by brodis01 from USA</td>
+            <td className='testimonials-reviews-td-author'>— TripAdvisor review by merrileej072052 from Houston</td>
           </tr>
         </table>
       </div>
